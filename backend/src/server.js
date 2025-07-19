675e254bfa4b5079e5877bd2464907fb28ebb646
Mohammad Sohail <sohail07277@gmail.com>
2025-07-19 21:51:25 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,22 +3,34 @@ import cors from "cors";
 import router from "./routes/notesRoutes.js"
 import {connectDB} from './config/db.js'
 import dotenv from 'dotenv';
+import path from 'path'
 import rateLimiter from './middleware/rateLimiter.js'
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const __dirname = path.resolve();
 
 connectDB();
 
-app.use(cors(
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors(
   {origin: "http://localhost:5173",}
 ))
+}
 app.use(express.json());
 app.use(rateLimiter)
 
 
 app.use("/api/notes" , router)
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+});
+}
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
